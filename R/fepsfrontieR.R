5a5e0ce51195c328aec813a8e5d8c2991dd9e1a0
clemenshaerder <clemens.haerder@gmail.com>
2018-08-05 13:54:40 +0200

    Added description to fepsfrontieR, sfmfep & SFM.generate including an example.

--- R/fepsfrontieR.R
+++ R/fepsfrontieR.R
@@ -1,21 +1,39 @@
-#' fepsfrontieR: A package for estimation of fixed-effect panel stochastic
-#' frontier models by within-model transformation as proposed by Wang & Ho.
-#' As proposed, Maximum Likelihood Estimation using nlminb().
+#' Estimating Fixed-Effects Panel Stochastic Frontier Models
+#'
+#' A package for estimation of fixed-effect panel stochastic
+#' frontier models by within-model transformation as proposed by Wang & Ho (2010).
+#' Maximum Likelihood Estimation is performed using nlminb( ).
 #' Confidence Intervals are provided using the Hessian Matrix or
-#' (time intensive) Bootstrapping to calculate Standard Error.
+#' numerically expensive Bootstrapping to calculate Standard Error.
 #' In addition, a Gibbs Sampler can be chosen as an alternative to MLE.
 #'
-#' The fepsfrontieR package provides three important functions:
-#' sfmfep, SFM.within and SFM.generate
+#' The fepsfrontieR package provides one important function
+#' sfmfep and a supplementary data generation function SFM.generate
 #'
 #' @section sfmfep:
-#' The sfmfep functions ...
+#' The sfmfep functions is the main function of the package fepsfrontieR.
+#' It provides two options.
+#' If "estimate = T" is chosen, the model is
+#' estimated with MLE or a Gibbs Sampler. Starting points can be provided
+#' for the numerical optimizer nlminb ( ). Otherwise OLS estimators are
+#' applied as starting points.
 #'
-#' @section SFM.within:
-#' The SFM.within functions ...
+#' If "estimate = F", a stochastic frontier model is calculated,
+#' based on provided point estimates.
+#'
+#' A S3 object is returned in both cases.
+#'
+#' @usage
+#' sfmfep(formula, data, group = NULL, N = NULL,
+#'        Time = NULL, mu = 0,  sigmaCI = 0.05, estimate = T,
+#'        myPar = c(sigma_u, sigma_v, beta = c( ), delta = c( )))
+#'
+#' SFM.generate(N, Time, beta, delta, sigma_u, sigma_v, mu_u = 0)
 #'
 #' @section SFM.generate:
-#' The SFM.generate functions ...
+#' The SFM.generate function generates a fixed-effects stochastic
+#' frontier model with exponential inefficencys. The amount of specified
+#' betas & deltas create associated explenatory varibles & inefficeny determinants.
 #'
 #' @docType package
 #' @name fepsfrontieR
--- R/sfm_generate.R
+++ R/sfm_generate.R
@@ -1,5 +1,11 @@
-#' SFM.generate creates a fixed-effects panel stochastic frontier model data set
-#' which can be used as examples for other functions of the package fepsfrontieR.
+#' SFM.generate creates a fixed-effects panel stochastic frontier model
+#'
+#' Data sets created by this function can be applied for sfmfep( ) of
+#' this package fepsfrontieR. The model specificiation are:
+#' alpha_i are fixed-effect parameters drawn from a uniform distribution in [0, 1].
+#' x_it is drawn from a normal distribution N(alpha_i, 1).
+#' z_it is drawn from a standard normal distribution.
+#' u_i (inefficency) is drawn from a truncated normal distribution with N(mu, sigma_u).
 #' @param N is an integer and specifies the amount of panels
 #' @param Time is an integer and specifies the amount of observations per panel
 #' @param beta is a vector of k estimates.
@@ -10,17 +16,20 @@
 #' of the stochastic inefficency
 #' @param sigma_v is postivie numeric and is the variation
 #' of the zero-mean random error.
-#' @param mu_u is numeric and is the mean of the truncated normal distribution
+#' @param mu is numeric and is the mean of the truncated normal distribution
 #' of the stochastic inefficency.
-#' @return A data.fram() including x, y, z & alpha variables
+#' @return A data.frame( ) including x, y, z & alpha variables
 #' @examples
-#' < an example >
+#' exampleSFM <- datsSFM.generate(N = 20, Time = 5, beta = c(0.5,0,9,3),
+#' delta = (0.5,0.1,2), sigma_u = 0.2, sigma_v = 0.1)
+#'
+#' exampleSFM
 #' @export
 
-SFM.generate <- function(N, Time, beta, delta, sigma_u, sigma_v, mu_u = 0){
+SFM.generate <- function(N, Time, beta, delta, sigma_u, sigma_v, mu = 0){
 
   if (!is.double(N) | !is.double(Time) | !is.vector(beta) | !is.vector(delta) |
-      !is.numeric(sigma_u) | !is.numeric(sigma_v)  | !is.numeric(mu_u) |
+      !is.numeric(sigma_u) | !is.numeric(sigma_v)  | !is.numeric(mu) |
       sigma_u <= 0 | sigma_v <= 0){
     stop ("Invalid input format of parameters.")
   }
@@ -29,7 +38,7 @@ SFM.generate <- function(N, Time, beta, delta, sigma_u, sigma_v, mu_u = 0){
   R <- length(delta)
 
   # Generate inefficencys for each panel and repeat it from truncated normal distribution (x>a)
-  u_star <- rep(truncnorm::rtruncnorm(N, a = 0, mean = mu_u, sd = sigma_u), each = Time)
+  u_star <- rep(truncnorm::rtruncnorm(N, a = 0, mean = mu, sd = sigma_u), each = Time)
 
   # Generate one alpha intercepts for each panel and repeat it from a uniform distribution
   alpha  <- rep(runif(N, 0, 1), each = Time)
