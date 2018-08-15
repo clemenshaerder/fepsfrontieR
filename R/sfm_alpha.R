e919231635694feaebf93841eb7a917c7fc8ddda
oliverdippel <oliverdippel@gmx.de>
2018-08-15 15:47:24 +0200

    for x with na`S

--- R/sfm_alpha.R
+++ R/sfm_alpha.R
@@ -1,36 +1,41 @@
-#' < Recovers the alpha (inteysfdysyxyssfdrcept) of each panel >
-#' @param par is a vector of regression coefficients & variance parameters.
-#'     1st parameter: sigma_u, 2nd parameter: sigma_v, followed by K beta & R delta coefficients
-#' @return < Describe what is returned when applying this functinon >
+#' < Recovers the individual fixed effects for each individuum >
+#' @param y is a n*t x 1 vector (response)
+#' @param x is a n*t x k matrix (explantatory variables)
+#' @param N an optional integer specifying the total amount of panels in the data set.
+#' @param Time an optional integer specifying the amount of observations per panel.
+#' @param mu is the mean of a truncated normal distribution of the stochastic inefficencys.
+#' @return recovered values of individual fixed effects
 #' @examples
 #' < an example >
 
 
-SFM.alpha <- function(y, x, beta, sigma_u, sigma_v, h, epsilon, N, Time, mu = 0){
+SFM.alpha <- function(y, x, beta, sigma_u, sigma_v, h, epsilon, N, Time, mu){
 
-  # TODO(Oli) comment the file according googles r comment standard (use -------- to easily readable code bloks)
   # TODO(Oli) add panelname to each alpha
-  ####-------------------####
+  #arrange date for better use
 
-  epsilon <- matrix(epsilon, ncol = N , nrow = Time)
+  epsilon <- matrix (epsilon, ncol = N , nrow = Time)
 
-  h <- matrix(h, ncol = N, nrow = Time)
-  h_mean <- apply(h, 2, mean)
+  h       <- matrix (h, ncol = N, nrow = Time)
+  h_mean  <- apply (h, 2, mean, na.rm = T)
 
-  y <- matrix(y, ncol = N, nrow = Time)
-  y_mean <- apply(y, 2, mean)
+  y       <- matrix (y, ncol = N, nrow = Time)
+  y_mean  <- apply (y, 2, mean, na.rm = T)
 
-  x.mat <- matrix(x, nrow = Time)
-  x_MEAN <- apply(x.mat, 2, mean)
-  x_mean <- matrix(x_MEAN, nrow = N)
-  pro_xb <- x_mean %*% beta
+  x.mat   <- matrix (x, nrow = Time)
+  x_MEAN  <- apply (x.mat, 2, mean, na.rm = T)
+  x_mean  <- matrix (x_MEAN, nrow = N)
+  pro_xb  <- x_mean %*% beta
 
-  ####--------------------####
 
-  mu_3star <- NULL; sigma_3star <- NULL; alpha <- NULL
+  #get sigma*** and mu*** ---------------------------
 
-  sum_eh <- apply(epsilon*h, 2, sum)
-  h_srt_sum <- apply(h^2, 2, sum)
+  mu_3star    <- NULL
+  sigma_3star <- NULL
+  alpha       <- NULL
+
+  sum_eh    <- apply (epsilon*h, 2, sum, na.rm = T)
+  h_srt_sum <- apply (h^2, 2, sum, na.rm = T)
 
   for(i in 1:N){
     mu_3star[i] <- (mu * sigma_u^-1 - sigma_v^-Time * sum_eh[i]) /
@@ -40,14 +45,18 @@ SFM.alpha <- function(y, x, beta, sigma_u, sigma_v, h, epsilon, N, Time, mu = 0)
       (h_srt_sum[i] + sigma_v^Time * sigma_u^-1)
   }
 
+  #take the square root of sigma*** since we only need this for alpha
   sigma_3star <- sqrt(sigma_3star)
 
+  #recover alpha for each N-----------------------------
   for(i in 1:N){
     alpha[i] <- y_mean[i] - pro_xb[i] + mu_3star[i] * h_mean[i]
     + sigma_3star[i] * h_mean[i] * (dnorm(mu_3star[i] / sigma_3star[i]) /
-                                      pnorm(mu_3star[i] / sigma_3star[i]))
+    pnorm(mu_3star[i] / sigma_3star[i]))
   }
 
   return(alpha)
 
 }
+
+
