47c8ac6b0b26b0ee47b033bf749b2ae76a69fc9a
oliverdippel <oliverdippel@gmx.de>
2018-08-16 12:14:23 +0200

    add unbalanced panels for ineff and alpha

--- R/sfm_inefficency_index.R
+++ R/sfm_inefficency_index.R
@@ -1,3 +1,4 @@
+
 #' < calculate the inefficiency of each individuum >
 #' @param par is a vector of regression coefficients & variance parameters.
 #'     1st parameter: sigma_u, 2nd parameter: sigma_v, followed by K beta & R delta coefficients
@@ -6,17 +7,17 @@
 #' < an example >
 
 SFM.inindex <- function(h, sigma2star, mu2star, N, Time){
-
+  
   # TODO(Oli) add panelname to each ineff.index
-
-  h        <- matrix(h, ncol = N, nrow = Time)
-  in_index <- matrix(rep(NA, N*Time), ncol = N, nrow = Time)
-
+  cumTime <- c(0, cumsum(Time))
+  h <- as.matrix(h)
+  in_index <- c()
+  
   for (i in 1:N){
-    in_index[, i] <- h[, i] * (mu2star[i] + (dnorm(mu2star[i] / sqrt(sigma2star[i])) *
-                    sqrt(sigma2star[i])) / pnorm(mu2star[i]/sqrt(sigma2star[i])))
+    in_index <- c(in_index, h[(cumTime[i] + 1) : cumTime[i + 1]] * (mu2star[i] + (dnorm(mu2star[i] / sqrt(sigma2star[i])) *
+                                                                                    sqrt(sigma2star[i])) / pnorm(mu2star[i]/sqrt(sigma2star[i]))))
   }
-
-  return (as.vector(in_index))
-
+  
+  return (in_index)
+  
 }
