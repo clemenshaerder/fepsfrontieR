b1f18839915ed571fc25ab4c1c268d403766b1b5
oliverdippel <oliverdippel@gmx.de>
2018-08-16 15:18:37 +0200

    ineff for both cases

---
+++ R/sfm.ineff.R
@@ -0,0 +1,23 @@
+#' < calculate the inefficiency of each individuum >
+#' @param par is a vector of regression coefficients & variance parameters.
+#'     1st parameter: sigma_u, 2nd parameter: sigma_v, followed by K beta & R delta coefficients
+#' @return < returns the inefficiency for each individuum >
+#' @examples
+#' < an example >
+
+SFM.inindex <- function(h, sigma2star, mu2star, N, Time){
+  
+  # TODO(Oli) add panelname to each ineff.index
+  
+  h <- matrix(h, ncol = N, nrow = Time)
+  in_index <- c()
+  
+  for (i in 1:N){
+    in_index <- c(in_index, h[,i] * 
+                 (mu2star[i] + (dnorm(mu2star[i] / sqrt(sigma2star[i])) *
+                  sqrt(sigma2star[i])) / pnorm(mu2star[i]/sqrt(sigma2star[i]))))
+  }
+  
+  return (in_index)
+  
+}
---
+++ R/sfm_ineffi_unbalanced.R
@@ -0,0 +1,26 @@
+#' < calculate the inefficiency of each individuum >
+#' @param par is a vector of regression coefficients & variance parameters.
+#'     1st parameter: sigma_u, 2nd parameter: sigma_v, followed by K beta & R delta coefficients
+#' @return < returns the inefficiency for each individuum >
+#' @examples
+#' < an example >
+
+SFM.inindex.unbalanced <- function(h, sigma2star, mu2star, N, Time){
+  
+  # TODO(Oli) add panelname to each ineff.index
+  cumTime <- c(0, cumsum(Time))
+  h <- as.matrix(h)
+  in_index <- c()
+  
+  for (i in 1:N){
+    in_index <- c(in_index, h[(cumTime[i] + 1) : cumTime[i + 1]] * 
+                 (mu2star[i] + (dnorm(mu2star[i] / sqrt(sigma2star[i])) *
+                  sqrt(sigma2star[i])) / pnorm(mu2star[i]/sqrt(sigma2star[i]))))
+  }
+  
+  return (in_index)
+  
+}
+
+
+
