c5872f34771ef6da26253fc3b457c4a9419cdfc1
oliverdippel <oliverdippel@gmx.de>
2018-08-29 20:04:22 +0200

    upadated summary

--- R/sfm.summary.R
+++ R/sfm.summary.R
@@ -1,6 +1,6 @@
 summary.sfmfep <- function(x, digits = max(3L, getOption("digits") - 3L), ...){
 
-  head1 <- c("Coefficients", "Std.Error", "t-value")
+  head1 <- c("Coefficients", "Std.Error", "T-stat")
   head2 <- c("alpha", "inefficiency")
 
   mat1 <- matrix(NA, length(x$contrasts), length(head1),
@@ -8,10 +8,12 @@ summary.sfmfep <- function(x, digits = max(3L, getOption("digits") - 3L), ...){
   mat2 <- matrix(NA, length(x$alpha), length(head2),
                  dimnames = list(rownames(x$alpha), head2))
 
+  T.stat <- x$coefficients / x$standerror
+  T.stat[c(1,2)] <- NA
 
   mat1[, 1] <- x$coefficients
   mat1[, 2] <- x$standerror
-  #mat1[, 3] <- x$tvalue
+  mat1[, 3] <- T.stat
   mat1 <- cbind(mat1, x$conf)
 
   mat2[, 1] <- x$alpha
@@ -27,7 +29,7 @@ summary.sfmfep <- function(x, digits = max(3L, getOption("digits") - 3L), ...){
   }
 
   if(!is.null(x$conf)){
-    cat("Coefficients:\t\t\t\tconfInterval\n")
+    cat("Coefficients:\t\t\t\t\tconfInterval\n")
     } else {
     cat("Coefficients:\n")
     }
