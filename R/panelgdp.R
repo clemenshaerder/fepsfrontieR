b43c7b1a8d32cfabbcbc2df088656b6fab6630cf
clemenshaerder <clemens.haerder@gmail.com>
2018-08-30 19:16:08 +0200

    added and updated description of panelgdp dataset

---
+++ R/panelgdp.R
@@ -0,0 +1,13 @@
+#' Stochastic Frontier Model of the GDP of countries
+#'
+#' @format A data frame with 60 rows and k+r+1 variables:
+#' \describe{
+#'   \item{country}{82 countries with observations each}
+#'   \item{code}{Each country has its own code}
+#'   \item{yr}{The year of the obseration, starting from 1 to 28}
+#'   \item{y}{...}
+#'   \item{k}{...}
+#'   \item{l}{...}
+#'   \item{h}{...}
+#' }
+"panelgdp"
