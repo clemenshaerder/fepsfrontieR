790710095020683735837dc037699a22cc23fdad
clemenshaerder <clemens.haerder@gmail.com>
2018-08-31 16:58:17 +0200

    minor stuff

--- R/sfm.print.R
+++ R/sfm.print.R
@@ -8,7 +8,7 @@
 print.sfmfep <- function(x, digits = max(3L, getOption("digits") - 3L), ...){
 
   if(x$estimate){
-    x$out <- setNames(x$coefficients, x$contrasts)
+    x$out <- setNames (x$coefficients, x$contrasts)
 
     cat("\nCall: ", "\n",
         paste (deparse (x$call), sep = "\n", collapse = "\n"), "\n\n", sep = "")
@@ -18,7 +18,7 @@ print.sfmfep <- function(x, digits = max(3L, getOption("digits") - 3L), ...){
     print.default (format(x$out, digits = digits),
                    print.gap = 2, quote = FALSE)
   } else {
-    cat("Likelihood:", deparse (round (x$objective, digits)))
+    cat ("Likelihood:", deparse (round (x$objective, digits)))
   }
 
 }
