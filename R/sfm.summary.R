0bb669783020162bb9a362ae7c7493c14b67761a
clemenshaerder <clemens.haerder@gmail.com>
2018-08-31 18:26:57 +0200

    changed x to object

--- R/sfm.summary.R
+++ R/sfm.summary.R
@@ -3,59 +3,59 @@
 #' the generic function summary which summarize objects
 #' returned by modelling functions.
 #' @param ... Additional arguments to the function
-#' @param x is a data frame of the inefficencys per panel.
+#' @param object is a data frame of the inefficencys per panel.
 #' @param digits defines the amount of digits for the values of the summary
 #' @export
 
-summary.sfmfep <- function(x, digits = max(3L, getOption("digits") - 3L), ...){
+summary.sfmfep <- function(object, digits = max(3L, getOption("digits") - 3L), ...){
 
-  if(x$estimate){
+  if(object$estimate){
 
     head2 <- c("alpha", "inefficiency")
 
-    mat2 <- matrix(NA, length(x$alpha), length(head2),
-                   dimnames = list(rownames(x$alpha), head2))
+    mat2 <- matrix(NA, length(object$alpha), length(head2),
+                   dimnames = list(rownames(object$alpha), head2))
 
-    mat2[, 1] <- x$alpha
-    mat2[, 2] <- x$Ineff
+    mat2[, 1] <- object$alpha
+    mat2[, 2] <- object$Ineff
 
-    if(!is.null(x$standerror)){
+    if(!is.null(object$standerror)){
 
       head1 <- c("Coefficients", "Std.Error")
 
-      mat1 <- matrix(NA, length(x$contrasts), length(head1),
-                   dimnames = list(x$contrasts, head1))
+      mat1 <- matrix(NA, length(object$contrasts), length(head1),
+                   dimnames = list(object$contrasts, head1))
 
-       mat1[, 1] <- x$coefficients
-       mat1[, 2] <- x$standerror
+       mat1[, 1] <- object$coefficients
+       mat1[, 2] <- object$standerror
 
-        if(!is.null(x$conf)){
-          mat1 <- cbind(mat1, x$conf)
+        if(!is.null(object$conf)){
+          mat1 <- cbind(mat1, object$conf)
         }
        } else {
 
          head1 <- c("Coefficients")
 
-         mat1 <- matrix(NA, length(x$contrasts), length(head1),
-                        dimnames = list(x$contrasts, head1))
+         mat1 <- matrix(NA, length(object$contrasts), length(head1),
+                        dimnames = list(object$contrasts, head1))
 
-         mat1[, 1] <- x$coefficients
-         if(!is.null(x$conf)){
-           mat1 <- cbind(mat1, x$conf)
+         mat1[, 1] <- object$coefficients
+         if(!is.null(object$conf)){
+           mat1 <- cbind(mat1, object$conf)
          }
       }
 
     cat("Call:\n",
-        paste(deparse(x$call), sep = "\n", collapse = "\n"), "\n\n", sep = "")
+        paste(deparse(object$call), sep = "\n", collapse = "\n"), "\n\n", sep = "")
 
-    if(x$bootstrap == T){
-      cat("method:", deparse(x$method), "\nEstimates are based on ",
-          x$B , "bootstrap samples\n\n")
+    if(object$bootstrap == T){
+      cat("method:", deparse(object$method), "\nEstimates are based on ",
+          object$B , "bootstrap samples\n\n")
     } else {
-      cat("method:", deparse(x$method), "\n\n")
+      cat("method:", deparse(object$method), "\n\n")
     }
 
-    if(!is.null(x$conf)){
+    if(!is.null(object$conf)){
       cat("Coefficients:\t\t\t\t\tconfInterval\n")
     } else {
       cat("Coefficients:\n")
@@ -63,13 +63,13 @@ summary.sfmfep <- function(x, digits = max(3L, getOption("digits") - 3L), ...){
 
     printCoefmat (mat1)
 
-    cat("\nlog.Likelihood:", deparse(round(x$objective, digits)),
-        "; AIC:", deparse(round(x$aic, digits)),
-        "; BIC:", deparse(round(x$bic, digits)), "\n\n")
+    cat("\nlog.Likelihood:", deparse(round(object$objective, digits)),
+        "; AIC:", deparse(round(object$aic, digits)),
+        "; BIC:", deparse(round(object$bic, digits)), "\n\n")
 
     printCoefmat (mat2)
   } else {
-    cat("Likelihood:", deparse(round(x$objective, digits)))
+    cat("Likelihood:", deparse(round(object$objective, digits)))
  }
 }
 
