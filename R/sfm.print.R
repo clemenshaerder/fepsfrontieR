62fb1e29608cd221aa94e58af8e4986b0b93ce96
clemenshaerder <clemens.haerder@gmail.com>
2018-08-31 16:52:14 +0200

    export

--- R/sfm.print.R
+++ R/sfm.print.R
@@ -3,6 +3,7 @@
 #' @param ... Additional arguments to the function
 #' @param x is a data frame of the inefficencys per panel.
 #' @param digits defines the amount of digits for the values of the summary
+#' @export
 
 print.sfmfep <- function(x, digits = max(3L, getOption("digits") - 3L), ...){
 
@@ -10,14 +11,14 @@ print.sfmfep <- function(x, digits = max(3L, getOption("digits") - 3L), ...){
     x$out <- setNames(x$coefficients, x$contrasts)
 
     cat("\nCall: ", "\n",
-        paste(deparse(x$call), sep = "\n", collapse = "\n"), "\n\n", sep = "")
+        paste (deparse (x$call), sep = "\n", collapse = "\n"), "\n\n", sep = "")
     cat("Coefficients")
 
     cat(":\n")
-    print.default(format(x$out, digits = digits),
+    print.default (format(x$out, digits = digits),
                   print.gap = 2, quote = FALSE)
   } else {
-    cat("Likelihood:", deparse(round(x$objective, digits)))
+    cat("Likelihood:", deparse (round (x$objective, digits)))
   }
 
 }
